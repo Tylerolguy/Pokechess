570adfcd56a6c6b682a81d25ecc82b72e084706b
Tyler King <94934780+Tylerolguy@users.noreply.github.com>
2025-12-02 01:20:09 -0500

    skeleton

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+package src;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import java.util.List;
+
+import org.json.*;
+
+public class Main extends JPanel {
+
+    
+    private static final int GRID_SIZE = 32;
+
+    public Main() {
+        setPreferredSize(new Dimension(800, 600));
+        setFocusable(true);
+
+    }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("PokeChess");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new Main());
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
